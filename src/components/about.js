fae99d6f96fa91e015a9f56f117492d67e9f4107
bruer <f.bruer@gmail.com>
2019-09-27 02:17:58 +0200

    new filename on an image

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,12 @@
 import React from "react"
 import styles from "./about.module.css"
-import bike from "../img/me-crop.jpg"
+import bike from "../img/me-on-a-bike.jpg"
 
 export default () => (
   <section className={styles.container} id="about">
     <h2>Hello,</h2>
     <div className={styles.infoBox}>
-      <img className={styles.image} src={bike} alt="me" />
+      <img className={styles.image} src={bike} alt="me on a bike" />
       <div>
         <h3>I'm Fredrik</h3>
         <p>former postman who nowadays study front end development</p>
